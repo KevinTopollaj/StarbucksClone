1e8c4b6781ba37bc7cadd47187b37baec9c039d6
Kevin Topollaj <kevintopollaj@gmail.com>
2022-05-13 18:52:48 +0200

    Remove the table view

--- StarbucksClone/StarbucksClone/Home/HomeViewController.swift
+++ StarbucksClone/StarbucksClone/Home/HomeViewController.swift
@@ -12,10 +12,6 @@ class HomeViewController: StarBucksViewController {
   let homeHeaderView = HomeHeaderView()
   var headerViewTopConstraint: NSLayoutConstraint?
   
-  var tableView = UITableView()
-  
-  let cellId = "cellId"
-  
   let tiles = [
     "Star balance",
     "Bonus star",
@@ -26,7 +22,6 @@ class HomeViewController: StarBucksViewController {
 
   override func viewDidLoad() {
     view.backgroundColor = .systemBackground
-    setupTableView()
     style()
     layout()
   }
@@ -42,78 +37,37 @@ extension HomeViewController {
   func style() {
     homeHeaderView.translatesAutoresizingMaskIntoConstraints = false
     homeHeaderView.backgroundColor = .systemGray4
-    tableView.translatesAutoresizingMaskIntoConstraints = false
   }
   
   func layout() {
     view.addSubview(homeHeaderView)
-    view.addSubview(tableView)
     
     headerViewTopConstraint = homeHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
     
     NSLayoutConstraint.activate([
       headerViewTopConstraint!,
       homeHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      homeHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      
-      tableView.topAnchor.constraint(equalTo: homeHeaderView.bottomAnchor),
-      tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+      homeHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
     ])
   }
   
 }
 
-// MARK: - TableView
-extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
-  
-  func setupTableView() {
-    tableView.dataSource = self
-    tableView.delegate = self
-    
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-    tableView.tableFooterView = UIView()
-  }
-  
-  // MARK: - UITableViewDataSource
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-    
-    cell.textLabel?.text = tiles[indexPath.row]
-    cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
-    
-    return cell
-  }
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return tiles.count
-  }
-  
-  // MARK: - UITableViewDelegate
-  
-  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 300
-  }
-  
-  func scrollViewDidScroll(_ scrollView: UIScrollView) {
-    let y = scrollView.contentOffset.y
-    
-    let swipingDown = y <= 0
-    let shouldSnap = y > 30
-    let labelHeight = homeHeaderView.greetingLabel.frame.height + 16
-    
-    UIView.animate(withDuration: 0.3) {
-      self.homeHeaderView.greetingLabel.alpha = swipingDown ? 1.0 : 0.0
-    }
-    
-    UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: []) {
-      self.headerViewTopConstraint?.constant = shouldSnap ? -labelHeight : 0
-      self.view.layoutIfNeeded()
-    }
-
-  }
-  
-}
+//func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//  let y = scrollView.contentOffset.y
+//  
+//  let swipingDown = y <= 0
+//  let shouldSnap = y > 30
+//  let labelHeight = homeHeaderView.greetingLabel.frame.height + 16
+//  
+//  UIView.animate(withDuration: 0.3) {
+//    self.homeHeaderView.greetingLabel.alpha = swipingDown ? 1.0 : 0.0
+//  }
+//  
+//  UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: []) {
+//    self.headerViewTopConstraint?.constant = shouldSnap ? -labelHeight : 0
+//    self.view.layoutIfNeeded()
+//  }
+//  
+//}
 
